d4799e3a4001161bce5b092a741829beb1430f46
Raul <raul.puscas86@gmail.com>
2023-04-15 19:48:33 +0300

    drawernavigator

---
+++ src/views/navigators/DrawerNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { View } from "react-native";
+import HomeScreen from "../screens/HomeScreen";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
