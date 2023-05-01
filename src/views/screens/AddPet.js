1177ac29cca9140d1743aae67baed46ca391b5f1
Raul <raul.puscas86@gmail.com>
2023-05-02 01:19:19 +0300

    APITara/Judet/Oras

--- src/views/screens/AddPet.js
+++ src/views/screens/AddPet.js
@@ -20,6 +20,8 @@ import { UserContext } from "../../context/AuthContext";
 import { useContext } from "react";
 import { addPet, addImage } from "../../data/Database";
 import { getUnixTime } from "date-fns"; // pentru data
+import { Dropdown } from "react-native-element-dropdown";
+import axios from "axios";
 
 const GENDER = {
   MASCUL: "Mascul",
@@ -33,6 +35,98 @@ const TIPURI_ANIMALE = {
 };
 
 const AddPet = ({ navigation }) => {
+  const [countryData, setCountryData] = useState([]);
+  const [stateData, setStateData] = useState([]);
+  const [cityData, setCityData] = useState([]);
+  const [country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  useEffect(() => {
+    var config = {
+      method: "get",
+      url: `https://api.countrystatecity.in/v1/countries`,
+      headers: {
+        "X-CSCAPI-KEY":
+          "NlFJbnd3Qnk5U2RZYmRMSElFZFBmZGQwZjNSSDQwaGFaWmoydlpjMg== ",
+      },
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let countryArray = [];
+        for (var i = 0; i < count; i++) {
+          countryArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setCountryData(countryArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleState = (countryCode) => {
+    var config = {
+      method: "get",
+      url: `https://api.countrystatecity.in/v1/countries/${countryCode}/states`,
+      headers: {
+        "X-CSCAPI-KEY":
+          "NlFJbnd3Qnk5U2RZYmRMSElFZFBmZGQwZjNSSDQwaGFaWmoydlpjMg== ",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let stateArray = [];
+        for (var i = 0; i < count; i++) {
+          stateArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setStateData(stateArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleCity = (countryCode, stateCode) => {
+    var config = {
+      method: "get",
+      url: `https://api.countrystatecity.in/v1/countries/${countryCode}/states/${stateCode}/cities`,
+      headers: {
+        "X-CSCAPI-KEY":
+          "NlFJbnd3Qnk5U2RZYmRMSElFZFBmZGQwZjNSSDQwaGFaWmoydlpjMg== ",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let cityArray = [];
+        for (var i = 0; i < count; i++) {
+          cityArray.push({
+            value: response.data[i].id,
+            label: response.data[i].name,
+          });
+        }
+        setCityData(cityArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   const [photo, setPhoto] = useState(null);
   const [errors, setErrors] = useState({});
   const authenticatedUser = useContext(UserContext);
@@ -191,7 +285,6 @@ const AddPet = ({ navigation }) => {
       navigation.navigate("Home", { numberOfPets });
     }
   };
-
   return (
     <ScrollView showsVerticalScrollIndicator="false">
       <View
@@ -410,7 +503,7 @@ const AddPet = ({ navigation }) => {
               </Pressable>
             </View>
           </View>
-          <CustomInput
+          {/* <CustomInput
             placeholder="Localitate+Județ"
             placeholderTextColor={COLORS.dark}
             style={{
@@ -426,7 +519,84 @@ const AddPet = ({ navigation }) => {
             onFocus={() => {
               handleError(null, "location");
             }}
-          />
+          /> */}
+
+          {/* DROPDOWN */}
+
+          <View style={styles.container}>
+            <View
+              style={{
+                borderColor: COLORS.primary,
+                borderRadius: 15,
+              }}
+            >
+              <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={countryData}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? "Tară" : "..."}
+                searchPlaceholder="Search..."
+                value={country}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={(item) => {
+                  setCountry(item.value);
+                  handleState(item.value);
+                  setIsFocus(false);
+                }}
+              />
+              <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={stateData}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? "Județ" : "..."}
+                searchPlaceholder="Search..."
+                value={state}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={(item) => {
+                  setState(item.value);
+                  handleCity(country, item.value);
+                  setIsFocus(false);
+                }}
+              />
+              <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={cityData}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? "Oraș" : "..."}
+                searchPlaceholder="Search..."
+                value={city}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={(item) => {
+                  setCity(item.value);
+                  setIsFocus(false);
+                }}
+              />
+            </View>
+          </View>
           <CustomInput
             placeholder="Despre animal"
             placeholderTextColor={COLORS.dark}
@@ -535,4 +705,46 @@ const AddPet = ({ navigation }) => {
 
 export default AddPet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginBottom: 15,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
